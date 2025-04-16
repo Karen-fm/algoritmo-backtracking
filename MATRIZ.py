6c2d433781998a07c74e4af6f1937719dfddc631
@karenferreiramachadodeoliveira <karenferreiramachadodeoliveira@gmail.com>
2025-04-16 16:38:35 -0300

    Adicionando código da atividade de backtracking

---
+++ MATRIZ.py
@@ -0,0 +1,21 @@
+#print("Olá, Mundo!")
+
+#nome = input("Qual é o seu nome? ")
+#print("Olá, " + nome + "!")
+
+#idade = input("Qual é a sua idade? ")
+#print("Você tem " + idade + " anos.")
+
+# heurística (menor distância do trabalho até em casa)
+
+# mapa matricial 
+
+
+mapa =  [
+        [0,0,0,0,0,0],
+        [0,0,0,0,0,0],
+        [0,0,0,0,0,0],
+        [0,0,0,0,0,0],
+        [0,0,0,0,0,0]
+
+]
